ccbcb82b6e44189bbd3817e804b5dcb0abc00c71
Kamran Raza <aipctoner@gmail.com>
2023-07-10 10:03:43 +0500

    ds

--- app.mjs
+++ app.mjs
@@ -1,23 +1,52 @@
 async function checkWeather(city) {
+	const city1 = document.getElementById("city1")
 	const key = "38a9ab72fa4044218e075627230807";
-	const url = "https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}&aqi=no";
-	
-  const temp = document.querySelector("#temp");
-  const img = document.querySelector("#img");
+	const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city1.value}&aqi=no`;
+
+	const temp = document.querySelector("#temp");
+	const temp_main = document.querySelector("#temp_main");
+	const cityName = document.querySelector("#cityName");
+	const img = document.querySelector("#img");
 
 	const weather_data = await fetch(`${url}`)
-		.then(Response => Response.json());
+		.then(Response => Response.json())
+		.then((data) => {
+			if (data.current.condition.code === 1006) {
+				document.querySelector(".container").innerHTML = data.message
+			}
+			console.log(data);
+			temp.innerHTML = `${data.current.temp_c}`;
+			temp_main.innerHTML = `${data.current.temp_c} <small
+			class="text-body-secondary fw-light"><sup>&deg;</sup>C</small>`;
+			cityName.innerHTML = data.location.name
 
-	temperature.innerHTML = `${weather_data.current.temp_c}`;
+		})
 
-	
+city1.value=""
+
 }
-const getWeatherData = () => {
-	const res = axios
-	  .get(`${BASE_URL}/v1/current.json?key=${key}&q=${city}&aqi=no`)
-	  .then((res) => console.log("res", res))
-	  .catch((err) => console.log("err", err));
-  };
+const  getWeatherData = async () => {
+	const key = "38a9ab72fa4044218e075627230807";
+	const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=karachi&aqi=no`;
+
+	const temp = document.querySelector("#temp");
+	const temp_main = document.querySelector("#temp_main");
+	const cityName = document.querySelector("#cityName");
+	const img = document.querySelector("#img");
+
+	const weather_data = await fetch(`${url}`)
+		.then(Response => Response.json())
+		.then((data) => {
+			console.log(data);
+			temp.innerHTML = `${data.current.temp_c}`;
+			temp_main.innerHTML = `${data.current.temp_c} <small
+			class="text-body-secondary fw-light"><sup>&deg;</sup>C</small>`;
+			cityName.innerHTML = data.location.name
+
+		})
+
+};
+getWeatherData()
 searchBtn.addEventListener(`click`, () => {
 	checkWeather();
 })
